4008db737551104931bf3c890d87720765860f7e
Stanley Umezuruike <manstylo@gmail.com>
2019-10-22 15:06:24 +0100

    Refactor!

--- app/src/main/java/com/example/animateam43/BMICalculations.kt
+++ app/src/main/java/com/example/animateam43/BMICalculations.kt
@@ -5,7 +5,7 @@ import kotlin.math.round
 fun main() {
     val comment = ("Body Mass Index (BMI) is your weight in kilograms divided by the square of your height in metres.\nKeep in mind that weight monitoring is for fitness and wellness ONLY, not for diagnosis or treatment of any medical condition.\nA healthcare professional should ALWAYS be consulted for check-up.")
     println(comment)
-    println(" ")
+    println(" ") //This does nothing really
 
     println("Height (cm): ")
     var height: Float = readLine()!!.toFloat()
@@ -16,7 +16,7 @@ fun main() {
     val W: Float = (weight)
 
     var calcB: Float = (W/(H*H))
-    var BMI:Float = (round(calcB * 100)/100)
+    var BMI:Float = round(calcB * 100)/100
 
     if (BMI <= 18.5){
         println("You are UNDERWEIGHT!\nYour BMI is " +BMI)
@@ -32,22 +32,5 @@ fun main() {
     if (BMI >= 30.0){
         println("You are OBESE!\nYour BMI is " + BMI)
     }
-
-    /*when
-    {
-        BMI <= 18.5 -> print("You are UNDERWEIGHT!\nYour BMI is " +BMI)
-        BMI >= (18.6..24.9) -> print("You are NORMAL!\nYour BMI is " +BMI)
-        BMI >= (25.0..29.9) -> print("You are OVERWEIGHT!\nYour BMI is " +BMI)
-        BMI >= 30.0 -> print("You are OBESE!\nYour BMI is " +BMI)
-        else -> print("Check your inputs and re-calculate!\nThank you!")
-
-    }*/
-    //println(round(BMI * 100)/100)
-    // for (x in 0 until 10) println(x) // Prints 0 through 9
-
-    /*for (BMI in 0 until 19) println("You are UNDERWEIGHT!")
-    for (BMI in 19 until 26) println("You are NORMAL!")
-    for (BMI in 26 until 31) println("You are OVERWEIGHT!")
-    for (BMI in 31 until 100) println("You are OBESE!")*/
 
 }
